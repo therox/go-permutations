a524191fbb82aa4fb92a0e2d8bf2834650ccfe59
Sergey Alipchenkov <stherox@gmail.com>
2017-09-08 23:41:04 +0300

    K-permutations funcs

---
+++ permutations.go
@@ -0,0 +1,72 @@
+package permutations
+
+import (
+	"sort"
+)
+
+// NewKPermutation generates initial permutation
+func NewKPermutation(data sort.Interface) {
+	sort.Sort(data)
+}
+
+// NextKPermutation generate next permutation
+func NextKPermutation(data sort.Interface, k int) bool {
+	n := data.Len()
+
+	var edge, j, i int
+	edge = k - 1
+
+	// find j in (k…n-1) where a[j] > a[edge]
+	j = k
+
+	for {
+		if (j < n) && (data.Less(j, edge)) {
+			j++
+		} else {
+			break
+		}
+	}
+
+	if j < n {
+		// swap a[egde], a[j]
+		data.Swap(edge, j)
+	} else {
+		// reverse a[k] to a[n-1]
+		for i, j = k, n-1; i < j; i, j = i+1, j-1 {
+			data.Swap(i, j)
+
+		}
+
+		// find rightmost ascent to left of edge
+		i = edge - 1
+		for {
+			if i >= 0 && data.Less(i+1, i) {
+				i--
+			} else {
+				break
+			}
+		}
+
+		if i < 0 {
+			return false // no more permutations
+		}
+
+		// find j in (n-1…i+1) where aj > ai
+		j = n - 1
+		for {
+			if j > i && data.Less(j, i) {
+				j--
+			} else {
+				break
+			}
+		}
+		// swap a[i], a[j]
+		data.Swap(i, j)
+
+		// reverse a[i+1] to an-1
+		for i, j = i+1, n-1; i < j; i, j = i+1, j-1 {
+			data.Swap(i, j)
+		}
+	}
+	return true
+}
---
+++ test/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+
+	permutations "github.com/therox/go-permutations"
+)
+
+type testStruct struct {
+	ID   int
+	Name string
+}
+
+type testStructSlice []testStruct
+
+func main() {
+	a := testStructSlice{
+		testStruct{
+			3, "Player 3",
+		},
+		testStruct{
+			2, "Player 2",
+		},
+		testStruct{
+			1, "Player 1",
+		},
+		testStruct{
+			4, "Player 4",
+		},
+	}
+	counter := 1
+	permutations.NewKPermutation(a)
+	for {
+		fmt.Println(counter, ": ", a[:3])
+		counter++
+		if !permutations.NextKPermutation(a, 3) {
+			break
+		}
+	}
+}
+
+func (is testStructSlice) Len() int {
+	return len(is)
+
+}
+
+// Less reports whether the element with
+// index i should sort before the element with index j.
+func (is testStructSlice) Less(i, j int) bool {
+	return is[i].ID < is[j].ID
+
+}
+
+// Swap swaps the elements with indexes i and j.
+func (is testStructSlice) Swap(i, j int) {
+	is[j], is[i] = is[i], is[j]
+}
